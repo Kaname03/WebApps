70740c21d80bfe043602981bade77e8b66f83b68
Kaname03 <131602058+Kaname03@users.noreply.github.com>
2023-05-03 21:50:28 +0900

    fix pwa

--- Dice/service-worker.js
+++ Dice/service-worker.js
@@ -1,14 +1,3 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('dice-roller-cache').then((cache) => {
-      return cache.addAll([
-        '/Dice/',
-        '/Dice/index.html',
-        '/Dice/style.css',
-        '/Dice/script.js',
-        '/Dice/icon-192x192.png',
-        '/Dice/icon-512x512.png',
-      ]);
-    })
-  );
+  console.log('Service Worker installed');
 });
