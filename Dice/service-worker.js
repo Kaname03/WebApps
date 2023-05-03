90f625ac62fc03b43a39b92c3eea29452ffefcf9
Kaname03 <131602058+Kaname03@users.noreply.github.com>
2023-05-03 22:46:57 +0900

    Update service-worker.js

--- Dice/service-worker.js
+++ Dice/service-worker.js
@@ -1,3 +1,48 @@
+const CACHE_NAME = 'interval-dice-cache-v1';
+const urlsToCache = [
+  '/',
+  '/src/script.js',
+  '/src/style.css',
+  '/src/sp-style.css',
+  '/favicon/128.png',
+  '/favicon/256.png',
+  '/favicon/512.png',
+];
+
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache:', CACHE_NAME);
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleting outdated cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
