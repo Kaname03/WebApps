58942f6766436d24932e801ff0a997d83bdd8ceb
Kaname03 <131602058+Kaname03@users.noreply.github.com>
2023-05-23 13:58:01 +0900

    add 500遠望金

--- money/script.js
+++ money/script.js
@@ -2,6 +2,7 @@ document.getElementById('calculator').addEventListener('input', function() {
   var note10000 = document.getElementById('note10000').value * 10000;
   var note5000 = document.getElementById('note5000').value * 5000;
   var note1000 = document.getElementById('note1000').value * 1000;
+  var coins500 = document.getElementById('coins500').value * 500 * 50;
   var coin500 = document.getElementById('coin500').value * 500;
   var coin100 = document.getElementById('coin100').value * 100 * 50;
   var coin50 = document.getElementById('coin50').value * 50 * 50;
@@ -12,7 +13,7 @@ document.getElementById('calculator').addEventListener('input', function() {
 
   var target = document.getElementById('target').value;
   
-  var total = note10000 + note5000 + note1000 + coin500 + coin100 + coin50 + coin10 + coin5 + coin1 + extraCoins;
+  var total = note10000 + note5000 + note1000 + coins500 + coin500 + coin100 + coin50 + coin10 + coin5 + coin1 + extraCoins;
   
   var resultDiv = document.getElementById('result');
   if (total === target) {
