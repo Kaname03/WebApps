2de3a64a5b197bc9670f0d504dbdbef8e1bfad75
Kaname03 <131602058+Kaname03@users.noreply.github.com>
2023-08-29 04:45:58 +0900

    PWAに対応

---
+++ money/service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'calculator-cache-v1';
+const urlsToCache = [
+  '/',
+  '/script.js',
+  '/style.css',
+  '/favicon.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache:', CACHE_NAME);
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleting outdated cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
